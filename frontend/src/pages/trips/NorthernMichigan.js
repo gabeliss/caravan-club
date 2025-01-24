73d3c6ed797f987c02c27b037808e8ce70a9bf70
Gabriel Liss <gabeliss@umich.edu>
2025-01-23 21:16:56 -0600

    update north mich trip page

--- frontend/src/pages/trips/NorthernMichigan.js
+++ frontend/src/pages/trips/NorthernMichigan.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './../../styles/itineraries.css';
+import { Link } from 'react-router-dom';
 
 function NorthernMichigan() {
 
@@ -9,16 +10,34 @@ function NorthernMichigan() {
         <div className="text-area">
           <h1 className="title">Northern Michigan</h1>
           <h1 className="subtitle">Great Lakes Getaway</h1>
-          <div className="text-content">
-            <p>
-              Discover the diverse landscapes of Northern Michigan, from the lively town of 
-              Traverse City to the historic charm of Mackinac Island and the breathtaking 
-              beauty of Pictured Rocks National Lakeshore. A CaraVan Club road trip lets you 
-              experience it all.
-            </p>
+        </div>
+      </div>
+
+      <div className="trip-info-container">
+        <div className="trip-info">
+          <h3>A Typical Trip to Northern Michigan</h3>
+          <div className="info-item">
+            <img src="https://caravan-bucket.s3.us-east-2.amazonaws.com/images/icons/calendar.png" alt="Calendar" />
+            <span className="info-label">Best Time to Visit</span>
+            <span className="info-value">May-October</span>
+          </div>
+          <div className="info-item">
+            <img src="https://caravan-bucket.s3.us-east-2.amazonaws.com/images/icons/clock.png" alt="Clock" />
+            <span className="info-label">Suggested Travel Time</span>
+            <span className="info-value">6 Nights</span>
+          </div>
+          <div className="info-item">
+            <img src="https://caravan-bucket.s3.us-east-2.amazonaws.com/images/icons/trail.png" alt="Trail" />
+            <span className="info-label">Local Activities</span>
+            <span className="info-value">Hiking, Kayaking, Exploring Towns</span>
+          </div>
+          <div className="info-item">
+            <img src="https://caravan-bucket.s3.us-east-2.amazonaws.com/images/icons/cabin.png" alt="Cabin" />
+            <span className="info-label">Typical Accommodations</span>
+            <span className="info-value">Tent , Glamping, Boutique Lodging</span>
           </div>
         </div>
-        <div className="image-area">
+        <div className="trip-info-image">
           <img 
             src="https://caravan-bucket.s3.us-east-2.amazonaws.com/images/trippage/northernmichigan/kayaking.png" 
             alt="Northern Michigan Landscape"
@@ -27,72 +46,61 @@ function NorthernMichigan() {
         </div>
       </div>
 
-      <div className="trip-info">
-        <h3>A Typical Trip to Northern Michigan</h3>
-        <div className="info-item">
-          <img src="https://caravan-bucket.s3.us-east-2.amazonaws.com/images/icons/calendar.png" alt="Calendar" />
-          <span className="info-label">Best Time to Visit</span>
-          <span className="info-value">May-October</span>
-        </div>
-        <div className="info-item">
-          <img src="https://caravan-bucket.s3.us-east-2.amazonaws.com/images/icons/clock.png" alt="Clock" />
-          <span className="info-label">Suggested Travel Time</span>
-          <span className="info-value">6 Nights</span>
-        </div>
-        <div className="info-item">
-          <img src="https://caravan-bucket.s3.us-east-2.amazonaws.com/images/icons/trail.png" alt="Trail" />
-          <span className="info-label">Local Activities</span>
-          <span className="info-value">Hiking, Kayaking, Exploring Towns</span>
-        </div>
-        <div className="info-item">
-          <img src="https://caravan-bucket.s3.us-east-2.amazonaws.com/images/icons/cabin.png" alt="Cabin" />
-          <span className="info-label">Typical Accommodations</span>
-          <span className="info-value">Tent , Glamping, Boutique Lodging</span>
-        </div>
+      <div className="text-content">
+        <h1>The Best of Northern Michigan</h1>
+        <p>
+          Explore the beatury of Northern Michigan on a customizable road trip with options for up to 6
+          nights, featuring all camping accomodations. This trip is perfect for those who want to immerse
+          themselves in the natural beatury and quaint towns of Michigan's most iconic destinations:
+          Traverse City, Mackinac City/Island, and Pictured Rocks National Lakeshore.
+        </p>
       </div>
-      <div className="maps-container">
-        <h3>Top Places to Visit in Northern Michigan</h3>
-        <div className="maps">
-          <div className="map">
-            <img src="https://caravan-bucket.s3.us-east-2.amazonaws.com/images/trippage/northernmichigan/map1.png" alt="Northern Michigan Map" />
+
+      <div className="best-stuff">
+        <div className="best-object">
+          <div className="best-title">Favorite Restaurant</div>
+          <div className="best-images">
+            <img 
+              src="https://caravan-bucket.s3.us-east-2.amazonaws.com/images/trippage/northernmichigan/kayaking.png" 
+              alt="Northern Michigan Landscape"
+              className="main-image"
+            />
           </div>
-          <div className="map">
-            <img src="https://caravan-bucket.s3.us-east-2.amazonaws.com/images/trippage/northernmichigan/map2.png" alt="Northern Michigan Map" />
+          <div className="best-answer">Farm Club</div>
+        </div>
+        <div className="best-object">
+          <div className="best-title">Best Hike</div>
+          <div className="best-images">
+            <img 
+              src="https://caravan-bucket.s3.us-east-2.amazonaws.com/images/trippage/northernmichigan/kayaking.png" 
+              alt="Northern Michigan Landscape"
+              className="main-image"
+            />
           </div>
+          <div className="best-answer">Empire Bluff Trail</div>
+        </div>
+        <div className="best-object">
+          <div className="best-title">Best Activity</div>
+          <div className="best-images">
+            <img 
+              src="https://caravan-bucket.s3.us-east-2.amazonaws.com/images/trippage/northernmichigan/kayaking.png" 
+              alt="Northern Michigan Landscape"
+              className="main-image"
+            />
+          </div>
+          <div className="best-answer">Sailing on Traverse Bay</div>
         </div>
       </div>
-      <div className="places-container">
-        <div className="place">
-            <div className="place-image">
-                <img src="https://caravan-bucket.s3.us-east-2.amazonaws.com/images/trippage/northernmichigan/lakemichigan.png" alt="Lake Michigan" />
-                <h4>Lake Michigan</h4>
-            </div>
-        </div>
-        <div className="place">
-            <div className="place-image">
-                <img src="https://caravan-bucket.s3.us-east-2.amazonaws.com/images/trippage/northernmichigan/fishtown.png" alt="Fish Town" />
-                <h4>Fish Town</h4>
-            </div>
-        </div>
-        <div className="place">
-            <div className="place-image">
-                <img src="https://caravan-bucket.s3.us-east-2.amazonaws.com/images/trippage/northernmichigan/empirebluffs.png" alt="Empire Bluffs" />
-                <h4>Empire Bluffs</h4>
-            </div>
-        </div>
-        <div className="place">
-            <div className="place-image">
-                <img src="https://caravan-bucket.s3.us-east-2.amazonaws.com/images/trippage/northernmichigan/traversecity.png" alt="Traverse City" />
-                <h4>Traverse City</h4>
-            </div>
-        </div>
-        <div className="place">
-            <div className="place-image">
-                <img src="https://caravan-bucket.s3.us-east-2.amazonaws.com/images/trippage/northernmichigan/picturedrocks.png" alt="Pictured Rocks" />
-                <h4>Pictured Rocks</h4>
-            </div>
-        </div>
+
+      <div className="outro-text">
+        Looking for more recommendations? <strong>Book your trip through Caravan Trip Plan to receive 
+        personalized suggestions</strong> for restaurants, activities, hikes with detailed trail information,
+        nearby towns, and more--complete with links to book activites and experiences!
       </div>
+      <Link to="/">
+        <button className='book-now-button'>Book Now</button>
+      </Link>
+
     </div>
   );
 }
