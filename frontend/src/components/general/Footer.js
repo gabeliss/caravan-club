28adca5a6d7d66d911cffca04556d6fc6a458ca3
Gabriel Liss <gabeliss@umich.edu>
2025-02-24 21:16:37 -0600

    fix footer

--- frontend/src/components/general/Footer.js
+++ frontend/src/components/general/Footer.js
@@ -5,15 +5,12 @@ function Footer() {
   return (
     <footer>
       <div className="footer-left">
-        <a href="mailto:caravantripplan@gmail.com">caravantripplan@gmail.com</a>
-        <a href="tel:+12489047411">(248) 904-7411</a>
-      </div>
-      <div className="footer-center">
         <Link to="/">
           <img src="https://caravan-bucket.s3.us-east-2.amazonaws.com/images/header/CaravanTripPlan.png" alt="Caravan Club Logo" />
         </Link>
       </div>
       <div className="footer-right">
+        <a href="mailto:caravantripplan@gmail.com">Email</a>
         <a href="https://www.instagram.com/caravantripplan" target="_blank" rel="noopener noreferrer">Instagram</a>
         <a href="https://www.tiktok.com" target="_blank" rel="noopener noreferrer">Tiktok</a>
       </div>
