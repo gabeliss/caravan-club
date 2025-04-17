a2f100b3538a49494c7e8cc63e7d1466ec623f27
Gabriel Liss <gabeliss@umich.edu>
2025-04-17 08:41:44 -0500

    rec.gov working

--- backend/app/caravan_automation/scrapers/mackinac_city/__init__.py
+++ backend/app/caravan_automation/scrapers/mackinac_city/__init__.py
@@ -6,9 +6,10 @@ This package contains web scrapers for campgrounds in the Mackinac City area.
 
 from .scrapeStIgnaceKoa import scrape_stIgnaceKoa_api
 from .scrapeIndianRiver import scrape_indianRiver_api
-
+from .scrapeStraitsStatePark import scrape_straitsStatePark
 
 __all__ = [
     'scrape_stIgnaceKoa_api',
-    'scrape_indianRiver_api'
+    'scrape_indianRiver_api',
+    'scrape_straitsStatePark'
 ] 
--- backend/app/caravan_automation/scrapers/traverse_city/__init__.py
+++ backend/app/caravan_automation/scrapers/traverse_city/__init__.py
@@ -1,5 +1,5 @@
-from .scrapeStraitsStatePark import scrape_straits_state_park
-from .scrapeMidnrReservations import scrape_midnr_reservations
+from .scrapeTraverseCityStatePark import scrape_traverse_city_state_park
+from ..scrapeMidnrReservations import scrape_midnr_reservations
 from .scrapeTraverseCityKoa import scrape_traverse_city_koa
 from .scrapeUncleDuckysPaddlersVillage import scrape_uncle_duckys_paddlers_village
 from .scrapeAnchorInn import scrape_anchor_inn
@@ -7,7 +7,7 @@ from .scrapeLeelanauPines import scrape_leelanau_pines
 from .scrapeTimberRidge import scrape_timber_ridge
 
 __all__ = [
-    'scrape_straits_state_park',
+    'scrape_traverse_city_state_park',
     'scrape_midnr_reservations',
     'scrape_traverse_city_koa',
     'scrape_uncle_duckys_paddlers_village',
